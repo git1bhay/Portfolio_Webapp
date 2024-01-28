498273b132d3fcf77753f461a724ad96070531cc
git1bhay <soniabhay797@gmail.com>
2024-01-28 18:27:22 +0530

    28/1 navbar

--- My_Portfolio/src/Components/NavBar/NavBar.jsx
+++ My_Portfolio/src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,19 @@
 export const NavBar = () => {
   return (
-    <div className='font-medium px-10 bg-black border space-x-5'>NavBar</div>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-xl">
+          Abhay
+        </div>
+        <div className="space-x-4">
+        <a href="#" className="text-white hover:text-gray-300">Home</a>
+          <a href="#" className="text-white hover:text-gray-300">About</a>
+          <a href="#" className="text-white hover:text-gray-300">Projects</a>
+          <a href="#" className="text-white hover:text-gray-300">Contact</a>
+        </div>
+
+      </div>
+
+    </nav>
   );
 };
